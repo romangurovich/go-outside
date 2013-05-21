20720f65a65adc73449cc96aa391b26baaffd881
Roman Gurovich <romangurovich@gmail.com>
2013-05-21 13:37:13 -0700

    Initial commit

---
+++ GoOutsideApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoOutsideApp::Application.config.secret_token = '42c70a43292dab4d4b8ab6a657fe2e66e8a5fd64f13b62029dc379ca114dd54a0e8e1aeacafb84bc38b7903bbe6ae09db206b83ab6f1e5879be80707d4463fdf'
